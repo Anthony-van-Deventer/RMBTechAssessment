0218ff0f7efcde25cd86e7089a4dc923044406db
Anthony van Deventer <anthony.vandeventer@standardbank.co.za>
2025-01-25 13:32:56 +0200

    improving matching engine tests

--- src/main/java/service/MatchingEngine.java
+++ src/main/java/service/MatchingEngine.java
@@ -3,7 +3,6 @@ package service;
 import model.Order;
 import model.Side;
 
-import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
@@ -20,11 +19,13 @@ public class MatchingEngine {
     }
 
     public void fillOrder(double price, int quantity, Side side) {
+        //looking to fill orders of the order side
         Side sideToFill = side == Side.BUY ? Side.SELL : Side.BUY;
         List<Order> existingOrders = orderBook.getOrdersByPriceAndSide(price, sideToFill);
-        if (existingOrders == null) {
+        if (existingOrders.isEmpty()) {
             orderBook.addOrder(price, quantity, side);
         } else {
+            //using an iterator to be able to remove from the collection while iterating over it
             Iterator<Order> orderListIterator = existingOrders.iterator();
             while (orderListIterator.hasNext()){
                 Order orderToCheck = orderListIterator.next();
@@ -35,8 +36,11 @@ public class MatchingEngine {
                 else {
                     int modifiedQty = orderToCheck.getQuantity() - quantity;
                     orderToCheck.setQuantity(modifiedQty);
+                    //quantity is smaller than the quantity of the matching order, so we can leave the function
+                    return;
                 }
             }
+            //if after filling the orders the new order still has quantity left then its added to the orderbook
             if (quantity>0) orderBook.addOrder(price, quantity, side);
         }
 
--- src/test/java/service/MatchingEngineTest.java
+++ src/test/java/service/MatchingEngineTest.java
@@ -24,29 +24,36 @@ class MatchingEngineTest {
     }
 
     @Test
-    void testFillOrderFullMatch() {
+    void whenPartiallyFillingTheOrder_shouldUpdatePartiallyFilledOrderToQuantityMinusFilledQuantity() {
         orderBook.addOrder(10.0, 100, Side.BUY);
         orderBook.addOrder(10.0, 100, Side.SELL);
 
-        matchingEngine.fillOrder(10.0, 100, Side.BUY);
+        matchingEngine.fillOrder(10.0, 50, Side.BUY);
 
-        assertTrue(orderBook.getOrdersByPriceAndSide(10.0, Side.SELL).isEmpty());
-        assertFalse(orderBook.getOrdersByPriceAndSide(10.0, Side.BUY).isEmpty());
+        assertEquals(50,orderBook.getOrdersByPriceAndSide(10.0, Side.SELL).get(0).getQuantity());
+
     }
 
     @Test
-    void testFillOrderPartialMatch() {
+    void whenFillingAnOrder_ShouldFillOrderCompletelyAndRemoveFilledOrder() {
         orderBook.addOrder(10.0, 100, Side.BUY);
-        orderBook.addOrder(10.0, 150, Side.SELL);
+        orderBook.addOrder(10.0, 150, Side.BUY);
 
-        matchingEngine.fillOrder(10.0, 100, Side.BUY);
+        matchingEngine.fillOrder(10.0, 100, Side.SELL);
 
-        assertEquals(50, orderBook.getOrdersByPriceAndSide(10.0, Side.SELL).get(0).getQuantity());
+        assertEquals(150, orderBook.getOrdersByPriceAndSide(10.0, Side.BUY).get(0).getQuantity());
+        assertEquals(1, orderBook.getOrdersByPriceAndSide(10.0, Side.BUY).size());
     }
 
     @Test
     void testFillOrderNoExistingOrders() {
         matchingEngine.fillOrder(15.0, 100, Side.BUY);
         assertEquals(1, orderBook.getOrdersByPriceAndSide(15.0, Side.BUY).size());
+    }
+
+    @Test
+    void whenNoOrdersAtMatchingPrice_shouldAddThatOrder() {
+        matchingEngine.fillOrder(1234.01, 100, Side.BUY);
+        assertEquals(1, orderBook.getOrdersByPriceAndSide(1234.01, Side.BUY).size());
     }
 }
